a22e3637c59b43fb5bc913f46e07079bb0be7e1a
dohpahmine <61051561+nelsongoh@users.noreply.github.com>
2020-03-30 03:16:53 +1100

    Added country abbreviations as part of the constants.

--- type_constants.js
+++ type_constants.js
@@ -1,4 +1,12 @@
 module.exports = {
-  UPDATE_GROUP_MESSAGE: '0',
-  REGISTER_GROUP_CHAT: '1'
+  // Callback query constants
+  'UPDATE_GROUP_MESSAGE': '0',
+  'REGISTER_GROUP_CHAT': '1',
+
+  // Country / region constants
+  'AU_NZ': 'au_nz', // Australia / New Zealand
+  'NA': 'na', // North America
+  'EU_UK': 'eu_uk', // Europe / United Kingdom,
+  'SG': 'sg', // Singapore
+  'ROTW': 'rotw', // Rest of the World
 }
