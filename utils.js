36c1574ffb3a49a8678d0b6ef9b631756fc33d7f
dohpahmine <61051561+nelsongoh@users.noreply.github.com>
2020-03-30 02:07:07 +1100

    Refactored the function which generates the possible group chat regions for selection in the callback query, to use the proper tags for regex recognition. Also uses constants now. Refactored some of the other functions which conduct regex recognition to use the constants.

--- utils.js
+++ utils.js
@@ -3,6 +3,43 @@ module.exports = {
 
   },
 
+  getCallbackType: (cbqData) => {
+    let regex = require('./regex');
+    return cbqData.match(regex.cbqType)[0]
+  },
+
+  getCallbackValue: (cbqData) => {
+    let regex = require('./regex');
+    return cbqData.match(regex.cbqValue)[0];
+  },
+
+  getCallbackGrpChatId: (cbqData) => {
+    let regex = require('./regex');
+    return cbqData.match(regex.cbqGrpChatId)[0];
+  },
+
+  generateSelectGrpChatMsgUpdate: (btnLabelValueObj) => {
+    const Types = require('./type_constants');
+    let outputObj = {};
+    for (let key in btnLabelValueObj) {
+      outputObj[key] = '[t]' + Types.UPDATE_GROUP_MESSAGE + '[/t]' + '[v]' + btnLabelValueObj[key] + '[/v]';
+    }
+    return outputObj;
+  },
+
+  generateRegGrpChatRegions: (grpChatId) => {
+    let Types = require('./type_constants');
+    let regions = {
+      'Singapore': '[t]' + Types.REGISTER_GROUP_CHAT + '[/t][v]sg[/v]' + '[gcid]' + grpChatId.toString() + '[/gcid]',
+      'Australia / New Zealand': '[t]' + Types.REGISTER_GROUP_CHAT + '[/t][v]au_nz[/v]' + '[gcid]' + grpChatId.toString() + '[/gcid]',
+      'Europe / United Kingdom': '[t]' + Types.REGISTER_GROUP_CHAT + '[/t][v]eu_uk[/v]' + '[gcid]' + grpChatId.toString() + '[/gcid]',
+      'North America': '[t]' + Types.REGISTER_GROUP_CHAT + '[/t][v]na[/v]' + '[gcid]' + grpChatId.toString() + '[/gcid]',
+      'Rest of the World': '[t]' + Types.REGISTER_GROUP_CHAT + '[/t][v]rotw[/v]' + '[gcid]' + grpChatId.toString() + '[/gcid]'
+    };
+
+    return regions;
+  },
+
   createInlineKeyboard: (btnLabelValueObj) => {
     let inlineKeyboardMarkup = {
       'inline_keyboard': []
