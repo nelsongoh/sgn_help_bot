acc75e0132444cdc115246e34a09134d3437e791
dohpahmine <61051561+nelsongoh@users.noreply.github.com>
2020-03-29 18:23:30 +1100

    Updated the spam filter and added some additional regexes that were used in the callback query matches.

--- regex.js
+++ regex.js
@@ -1,4 +1,5 @@
 module.exports = {
+  // Regex for general public commands
   cmdStart: /^\/start$/i,
   cmdHelp: /^\/help$/i,
   cmdInfo: /^\/info$/i,
@@ -6,13 +7,20 @@ module.exports = {
   cmdFltStatus: /^\/fltstatus$/i,
   cmdChats: /^\/chats$/i,
   cmdGetCases: /^\/getcases$/i,
+  // Regex for admin commands
   adminCmdRegGrpChat: /^\/reggrpchat$/i,
+  // Regex for matching values in callback queries
+  cbqType: /(?<=<cbq_type>)(.*)(?=<\/cbq_type>)/g,
+  cbqValue: /(?<=<value>)(.*)(?=<\/value>)/g,
+  // Regex for the spam filter
   spamFilter: new RegExp([
     '/',
     '((?=(.|\n)*👇)(?=(.|\n)*bitcoin)(?=(.|\n)*http))', '|',
     '((?=(.|\n)*👇)(?=(.|\n)*crypto)(?=(.|\n)*http))', '|',
     '((?=(.|\n)*👇)(?=(.|\n)*invest)(?=(.|\n)*http))', '|',
-    '((?=(.|\n)*invest)(?=(.|\n)*profit)(?=(.|\n)*http))',
+    '((?=(.|\n)*invest)(?=(.|\n)*profit)(?=(.|\n)*http))', '|',
+    '(?=(.|\n)*AAAAAFY3TYmivxjQejlZPA)', '|',
+    '(?=(.|\n)*AAAAAFDE45as4-4bvyhcYg)',
     '/i'
   ].join('')),
 };
