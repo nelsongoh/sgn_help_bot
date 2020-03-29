23e01a647d17c2cee98c6a65babf5682422a8d3f
dohpahmine <61051561+nelsongoh@users.noreply.github.com>
2020-03-30 02:04:51 +1100

    Updated the callback query functions to handle callbacks from the registration of new chat groups, specifically, the regions to assign these groups to. Also some minor refactoring to use constants from a type_constants file.

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const DataPull = require('./data-pull');
 const covidCases = require('./covid-cases');
 const groupAdmin = require('./grp-chat');
 const regex = require('./regex');
+const Types = require('./type_constants');
 
 const uncleLeeBot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN);
 uncleLeeBot.setWebHook(process.env.WEBHOOK_URL + "/" + process.env.WEBHOOK_TOKEN);
@@ -149,14 +150,34 @@ app.get("/covid-19/au/fetch", (req, res) => {
 
 // The listener for potential spam messages from user
 uncleLeeBot.onText(regex.spamFilter, (msg) => {
+  // We kick the user out first upon spam detection
   uncleLeeBot.kickChatMember(msg.chat.id, msg.from.id).then((isSuccess) => {
     // If there is an issue with kicking the user, notify me
-    if (!isSuccess) {
+    if (isSuccess) {
       uncleLeeBot.sendMessage(
         22595307,
-        "Ah boy ah, there was a problem kicking the spammer from the chat."
+        "Ah boy, I've made a kick from a group chat, here's the details:\n\n" +
+        "Group chat title: " + msg.chat.title + "\n" +
+        "Kicked user: " + msg.from.first_name + " " + msg.from.last_name + "\n" +
+        " (" + msg.from.username + ")" + "\n" +
+        "For spamming: " + msg.text
       )
     }
+  })
+  // Then we clean up the message from the group chat
+  .then(() => {
+    uncleLeeBot.deleteMessage(msg.chat.id, msg.message_id);
+  })
+  // If there's an issue with kicking the user, notify me
+  .catch((err) => {
+    uncleLeeBot.sendMessage(
+      22595307,
+      "Ah boy ah, there was a problem trying to kick the spammer out. Here's the details:\n\n" +
+      "Group chat title: " + msg.chat.title + "\n" +
+      "User attempted to kick: " + msg.from.first_name + " " + msg.from.last_name + 
+      " (" + msg.from.username + ")" + "\n" +
+      "Reason: " + err
+    )
   })
 })
 
@@ -164,136 +185,182 @@ uncleLeeBot.onText(regex.spamFilter, (msg) => {
                                 ADMIN-RELATED COMMANDS FROM HERE
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-// This is when the user inputs their group message update
-// uncleLeeBot.onText(/\*/, (msg) => {
-
-// });
+// To add a group chat to the datastore
+uncleLeeBot.onText(regex.adminCmdRegGrpChat, (msg) => {
+  // Check to make sure if it's me
+  if (msg.from.id === 22595307) {
+    // Create an entry in the datastore for the group chat, without the region first
+    groupAdmin.regGrpChat(msg.chat.id, msg.chat.title).then((isSuccess) => {
+      if (isSuccess) {
+        // Generate the group chat regions for registering a group chat, with the group chat ID attached
+        let keyboardData = Utils.generateRegGrpChatRegions(msg.chat.id);
+      
+        // Ask which region to add this chat to
+        uncleLeeBot.sendMessage(
+          22595307,
+          "Ah boy ah, which region do you want this group chat classified as?",
+          {
+            'reply_markup': Utils.createInlineKeyboard(keyboardData)
+          }
+        );
+      }
+    });
+  }
+});
 
 // This is when a callback query is received, from the pressing of the chat region buttons
 uncleLeeBot.on('callback_query', (cbq) => {
-  // Here we get the callback query with its ID, and the callback data (groupChatId)
-  // We update the database with the callback data, i.e. What group chat ID's message to change
-  let userId = cbq.from.id;
-  groupAdmin.updateChatIdRef(userId, cbq.data).then((ifSuccess) => {
-    let followUpMsg = "";
+  // Split the data received into a callback type, and the actual callback value
+  let cbqType = Utils.getCallbackType(cbq.data);
+  let cbqValue = Utils.getCallbackValue(cbq.data);
 
-    // If we successfully update the chat ID that we need to
-    if (ifSuccess) {
-      followUpMsg = "Group chat selected!";
-    }
-    else {
-      followUpMsg = "ERROR: Something went wrong!";
-    }
+  if (cbqType === Types.UPDATE_GROUP_MESSAGE) {
+    // Here we get the callback query with its ID, and the callback data (groupChatId)
+    // We update the database with the callback data, i.e. What group chat ID's message to change
+    let userId = cbq.from.id;
+    groupAdmin.updateGrpChatIdRef(userId, cbqValue).then((ifSuccess) => {
+      let followUpMsg = "";
 
-    return followUpMsg;
-  })
-  .then((followUpMsg) => {
-    // We answer the callback query (MANDATORY!)
-    uncleLeeBot.answerCallbackQuery(
-      cbq.id,
-      {
-        'text': followUpMsg,
-      }  
-    )
-  })
-  .then(() => {
-    // We send another message to the user asking them for the group message
-    // that they want to set now
-    uncleLeeBot.sendMessage(
-      userId,
-      Messages.PROMPT_ADMIN_NEW_MSG,
-      {
-        'reply_markup': {
-          'force_reply': true
-        }
+      // If we successfully update the chat ID that we need to
+      if (ifSuccess) {
+        followUpMsg = "Group chat selected!";
       }
-    )
-    // Then we attach a listener to listen for a reply from the user 
-    .then((sentMsg) => {
-      let listenerId = uncleLeeBot.onReplyToMessage(
-        sentMsg.chat.id,
-        sentMsg.message_id,
-        // This is the callback function which triggers upon the user's reply
-        (userReply) => {
-          // We will need to update the message for the group chat in the DB, 
-          groupAdmin.updateGrpChatMsg(userReply.from.id, userReply.text)
-            .then((isSuccess) => {
-              let outputMsg = "";
-              if (isSuccess) {
-                outputMsg = "Done! The group chat message has been updated!"
-              }
-              else {
-                outputMsg = "Uh oh, something went wrong with the update!"
-              }
+      else {
+        followUpMsg = "ERROR: Something went wrong!";
+      }
 
-              // Let the user know that the outcome of the message update
-              uncleLeeBot.sendMessage(
-                userId,
-                outputMsg
-              )
-            })
-            // Then we reset the group chat ID whose message needed to be changed
-            // i.e. There is no group chat whose message needs changing now, already changed
-            .then(() => {
-              groupAdmin.updateChatIdRef(userReply.from.id, -1)
-                .then((isSuccess) => {
-                  // If there's an issue, notify me
-                  if (!isSuccess) {
+      return followUpMsg;
+    })
+    .then((followUpMsg) => {
+      // We answer the callback query (MANDATORY!)
+      uncleLeeBot.answerCallbackQuery(
+        cbq.id,
+        {
+          'text': followUpMsg,
+        }  
+      );
+    })
+    .then(() => {
+      // We send another message to the user asking them for the group message
+      // that they want to set now
+      uncleLeeBot.sendMessage(
+        userId,
+        Messages.PROMPT_ADMIN_NEW_MSG,
+        {
+          'reply_markup': {
+            'force_reply': true
+          }
+        }
+      )
+      // Then we attach a listener to listen for a reply from the user 
+      .then((sentMsg) => {
+        let listenerId = uncleLeeBot.onReplyToMessage(
+          sentMsg.chat.id,
+          sentMsg.message_id,
+          // This is the callback function which triggers upon the user's reply
+          (userReply) => {
+            // We will need to update the message for the group chat in the DB, 
+            groupAdmin.updateGrpChatMsg(userReply.from.id, userReply.text)
+              .then((isSuccess) => {
+                let outputMsg = "";
+                if (isSuccess) {
+                  outputMsg = "Done! The group chat message has been updated!"
+                }
+                else {
+                  outputMsg = "Uh oh, something went wrong with the update!"
+                }
+
+                // Let the user know that the outcome of the message update
+                uncleLeeBot.sendMessage(
+                  userId,
+                  outputMsg
+                )
+              })
+              // Then we reset the group chat ID whose message needed to be changed
+              // i.e. There is no group chat whose message needs changing now, already changed
+              .then(() => {
+                groupAdmin.updateGrpChatIdRef(userReply.from.id, -1)
+                  .then((isSuccess) => {
+                    // If there's an issue, notify me
+                    if (!isSuccess) {
+                      uncleLeeBot.sendMessage(
+                        22595307,
+                        "There was an issue with resetting the group chat ID whose message needed to be changed."
+                      )
+                    }
+                  })
+              })
+              // Then we need to detach the listener from Uncle Lee
+              .then(() => {
+                // Get the listener ID
+                groupAdmin.getListenerId(userReply.from.id)
+                  .then((botListenerId) => {
+                    // If we have no issues
+                    if (botListenerId !== false) {
+                      // Detach the listener from Uncle Lee
+                      uncleLeeBot.removeReplyListener(botListenerId);
+                    }
+                    else {
+                      throw "Issue with retrieving the listener ID."
+                    }
+                  })
+                  // Then if there are no issues, we continue with resetting the listener ID in the database
+                  .then(() => {
+                    // Reset the listener ID
+                    groupAdmin.storeListenerId(userReply.from.id, -1)
+                  })
+                  .catch((err) => {
                     uncleLeeBot.sendMessage(
                       22595307,
-                      "There was an issue with resetting the group chat ID whose message needed to be changed."
+                      err
                     )
-                  }
-                })
-            })
-            // Then we need to detach the listener from Uncle Lee
-            .then(() => {
-              // Get the listener ID
-              groupAdmin.getListenerId(userReply.from.id)
-                .then((botListenerId) => {
-                  // If we have no issues
-                  if (botListenerId !== false) {
-                    // Detach the listener from Uncle Lee
-                    uncleLeeBot.removeReplyListener(botListenerId);
-                  }
-                  else {
-                    throw "Issue with retrieving the listener ID."
-                  }
-                })
-                // Then if there are no issues, we continue with resetting the listener ID in the database
-                .then(() => {
-                  // Reset the listener ID
-                  groupAdmin.storeListenerId(userReply.from.id, -1)
-                })
-                .catch((err) => {
-                  uncleLeeBot.sendMessage(
-                    22595307,
-                    err
-                  )
-                })
-            })
-            .catch((err) => {
-              console.log(err);
-            })
-        }
-      );
-
-      // We need to store the listenerId so that the listener can be removed
-      groupAdmin.storeListenerId(userId, listenerId)
-        .then((isSuccess) => {
-          if (!isSuccess) {
-            uncleLeeBot.sendMessage(
-              userId,
-              "Uh oh, we had an issue storing the listener ID."
-            )
+                  })
+              })
+              .catch((err) => {
+                console.log(err);
+              })
           }
-        });
+        );
+
+        // We need to store the listenerId so that the listener can be removed
+        groupAdmin.storeListenerId(userId, listenerId)
+          .then((isSuccess) => {
+            if (!isSuccess) {
+              uncleLeeBot.sendMessage(
+                userId,
+                "Uh oh, we had an issue storing the listener ID."
+              )
+            }
+          });
+      });
+    })
+  }
+  else if (cbqType === Types.REGISTER_GROUP_CHAT) {
+    let grpChatId = Utils.getCallbackGrpChatId(cbq.data);
+
+    // Update the chat's region in the datastore
+    groupAdmin.updateGrpChatRegion(grpChatId, cbqValue).then((isSuccess) => {
+      let followUpMsg = "";
+      if (isSuccess) {
+        followUpMsg = "SUCCESS! Chat group registered!"
+      }
+      else {
+        followUpMsg = "ERROR: There was a problem registering the group chat."
+      }
+
+      // We answer the callback query (MANDATORY!)
+      uncleLeeBot.answerCallbackQuery(
+        cbq.id,
+        {
+          'text': followUpMsg,
+        }  
+      );
     });
-  })
+  }
 });
 
 // The UPDATE GROUP MESSAGE command
-uncleLeeBot.onText(/^\/updategrpmsg$/, (msg) => {
+uncleLeeBot.onText(regex.adminUpdateGrpMsg, (msg) => {
   // We need to retrieve the group chats that the user is allowed to change messages for
   groupAdmin.getAuthGrpChats(msg.from.id).then((permittedChats) => {
     // If we get an empty set back, then we know the user has no matching authorization
@@ -306,11 +373,15 @@ uncleLeeBot.onText(/^\/updategrpmsg$/, (msg) => {
     // Else if they do
     else {
       // Send them buttons to select which group chat they would like to update their message for
+      
+      // Edit the object to include a callback type for the listener to recognize
+      let keyboardCallback = Utils.generateSelectGrpChatMsgUpdate(permittedChats);
+
       uncleLeeBot.sendMessage(
         msg.from.id,
         "Please select the group chat for which the message should be updated:",
         {
-          'reply_markup': Utils.createInlineKeyboard(permittedChats),
+          'reply_markup': Utils.createInlineKeyboard(keyboardCallback),
         }
       )
     }
@@ -318,7 +389,7 @@ uncleLeeBot.onText(/^\/updategrpmsg$/, (msg) => {
 });
 
 // The FORCE COVID UPDATE command
-uncleLeeBot.onText(/^\/forcecovidupdate$/, (msg) => {
+uncleLeeBot.onText(regex.adminForceCovidUpdate, (msg) => {
   if (msg.from.id === 22595307) {
     // Trigger the pull to update the COVID cases into Firestore
     DataPull.getCovidUpdatesAu().then((isUpdateSuccessful) => {
@@ -341,7 +412,7 @@ uncleLeeBot.onText(/^\/forcecovidupdate$/, (msg) => {
 });
 
 // The DIAGNOSTICS command
-uncleLeeBot.onText(/\/diagnostics (.+)/, (msg, match) => {
+uncleLeeBot.onText(regex.adminRunDiagnostics, (msg, match) => {
   if (msg.from.id === 22595307) {
     uncleLeeBot.sendMessage(
       22595307,
@@ -356,7 +427,7 @@ uncleLeeBot.onText(/\/diagnostics (.+)/, (msg, match) => {
 });
 
 // The ID ME command
-uncleLeeBot.onText(/^\/idme$/, (msg) => {
+uncleLeeBot.onText(regex.cmdIdMe, (msg) => {
   uncleLeeBot.sendMessage(
     22595307,
     "Hello ah boy! Someone has tried getting themselves ID-ed:\n\n" +
