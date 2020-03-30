f0e72aee65c361ff1f6b210ec64fa3b2a1daa24d
dohpahmine <61051561+nelsongoh@users.noreply.github.com>
2020-03-31 03:12:00 +1100

    Added a new constant for the Telegram SGN channel title.

--- type_constants.js
+++ type_constants.js
@@ -9,4 +9,13 @@ module.exports = {
   'EU_UK': 'eu_uk', // Europe / United Kingdom,
   'SG': 'sg', // Singapore
   'ROTW': 'rotw', // Rest of the World
+
+  // Telegram group chat type constants
+  'TELEGRAM_GRP_CHAT_PVT': 'private',
+  'TELEGRAM_GRP_CHAT_GRP': 'group',
+  'TELEGRAM_GRP_CHAT_SUPER_GRP': 'supergroup',
+  'TELEGRAM_GRP_CHAT_CHN': 'channel',
+
+  // Official Singapore Global Network Channel link
+  'OFFICIAL_SGN_CHN_TITLE': 'Singapore Global Network Channel 🇸🇬',
 }
