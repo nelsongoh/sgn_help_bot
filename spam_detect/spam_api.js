ba3880e755324892deeee96d1926c9217a94cf54
dohpahmine <61051561+nelsongoh@users.noreply.github.com>
2020-04-11 20:14:16 +1000

    Wrote an additional API call to run all bot-administrated chat messages, through a spam detection machine learning model, which would return a binary value of whether this was detected as spam, or not.

---
+++ spam_detect/spam_api.js
@@ -0,0 +1,28 @@
+module.exports = {
+  detectMessage: (msg) => {
+    let axios = require('axios').default;
+    return axios({
+      method: "post",
+      url: process.env.DETECT_SPAM_FUNC_URL, 
+      data: { usr_msg: msg },
+      "headers": {"Content-Type": "application/json"},
+      validateStatus: ((status) => {
+        return true;
+      })
+    })
+    .then((resp) => {
+      // If we get the "I'm a teapot" status, this is spam
+      if (resp.status == 418) {
+        return true
+      }
+      // Else if we get a "OK, no content" status, it's a ham message
+      else if (resp.status == 204) {
+        return false
+      }
+      // If we receive any other status message, we throw an error
+      else {
+        throw "We're getting a different status from the spam detection API: " + resp.status
+      }
+    });
+  }
+}
