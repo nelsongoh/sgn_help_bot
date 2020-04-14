211f9c4e6b5607d47db949d7358c2d1a7e822134
dohpahmine <61051561+nelsongoh@users.noreply.github.com>
2020-04-14 17:57:34 +1000

    Created functions to store and remove group chat members' information on entry and exit into / from a group chat.

--- grp-chat.js
+++ grp-chat.js
@@ -1,4 +1,111 @@
 module.exports = {
+  removeGrpChatMember: (grpChatId, exGrpChatUser) => {
+    let Firestore = require('@google-cloud/firestore');
+    let db = new Firestore();
+
+    let grpChatMembRef = db.collection('grpChatMembers').doc((user.id).toString());
+
+    return db.runTransaction((t) => {
+      return t.get(grpChatMembRef)
+        .then((doc) => {
+          // If the document doesn't exist, i.e. We never got this member's details,
+          // We'll store information about their account
+          if (!doc.exists) {
+            let userUpdate = {
+              first_name: exGrpChatUser.first_name
+            }
+
+            if (typeof exGrpChatUser.last_name !== 'undefined') {
+              userUpdate['last_name'] = exGrpChatUser.last_name;
+            }
+            else {
+              userUpdate['last_name'] = '';
+            }
+
+            if (typeof exGrpChatUser.username !== 'undefined') {
+              userUpdate['username'] = exGrpChatUser.username;
+            }
+            else {
+              userUpdate['username'] = '';
+            }
+
+            t.set(grpChatMembRef, userUpdate);
+          }
+          // Else if the member exists, we just remove their membership from that group chat
+          else {
+            let currGrpChatMembership = doc.data().grpChats;
+            // If the membership exists in their records, we remove it and update it, else we do nothing
+            if (currGrpChatMembership.hasOwnProperty(grpChatId.toString())) {
+              delete currGrpChatMembership[grpChatId.toString()];
+              console.log(currGrpChatMembership);
+              t.set(grpChatMembRef, {'grpChats': currGrpChatMembership});
+            }
+          }
+        });
+    })
+    .then((result) => {
+      return true;
+    })
+    .catch((err) => {
+      console.log(err);
+      return false;
+    });
+  },
+
+  storeGrpChatMembers: (grpChatId, grpChatTitle, grpChatUsers) => {
+    let Firestore = require('@google-cloud/firestore');
+    let db = new Firestore();
+
+    let promises = grpChatUsers.map((user) => {
+      let grpChatMembRef = db.collection('grpChatMembers').doc((user.id).toString());
+      let userUpdate = {
+        first_name: user.first_name
+      }
+      
+      if (typeof user.last_name !== 'undefined') {
+        userUpdate['last_name'] = user.last_name;
+      }
+      else {
+        userUpdate['last_name'] = '';
+      }
+
+      if (typeof user.username !== 'undefined') {
+        userUpdate['username'] = user.username;
+      }
+      else {
+        userUpdate['username'] = '';
+      }
+
+      return db.runTransaction((t) => {
+        return t.get(grpChatMembRef)
+          .then((doc) => {
+            // If the document doesn't exist, we initialize it
+            if (!doc.exists) {
+              userUpdate['grpChats'] = {};
+              userUpdate['grpChats'][grpChatId.toString()] = grpChatTitle;
+            }
+            // Else if it does exist, we just update it
+            else {
+              let currGrpChatMembership = doc.data().grpChats;
+              currGrpChatMembership[grpChatId.toString()] = grpChatTitle;
+              userUpdate['grpChats'] = currGrpChatMembership;
+            }
+            // Then we update the entry
+            t.set(grpChatMembRef, userUpdate);
+          });
+      });
+    });
+
+    return Promise.all(promises)
+      .then((result) => {
+        return true;
+      })
+      .catch((err) => {
+        console.log(err);
+        return false;
+      });
+  },
+
   getGrpChatNameWithId: (grpChatId) => {
     let Firestore = require('@google-cloud/firestore');
     let db = new Firestore();
